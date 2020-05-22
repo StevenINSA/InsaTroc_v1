48584a3212ebb28033789f4bd34a7fd685f0d1b4
Noëline <noeline.migeon@gmail.com>
2020-05-22 16:13:01 +0200

    Ajout username modifyUserInfo (j'avais oublié)

--- insatroc/Back/app.js
+++ insatroc/Back/app.js
@@ -610,7 +610,7 @@ app.post('/modifyUserInfo', (req, res, next) => {
       console.log("username already exists")
       res.status(401).json({"message" : "username already exists"});
     } else { //sinon, mise à jour de la base de données
-      con.query("UPDATE Student SET Name='"+req.body.firstname+"', TelephoneNumber='"+req.body.phone+"', Address='"+req.body.other+"', Surname='"+req.body.lastname+"' WHERE StudentID='"+userID+"'",function (err, result, fields) {
+      con.query("UPDATE Student SET Username='"+req.body.username+"', Name='"+req.body.firstname+"', TelephoneNumber='"+req.body.phone+"', Address='"+req.body.other+"', Surname='"+req.body.lastname+"' WHERE StudentID='"+userID+"'",function (err, result, fields) {
         if (err) throw err;
         res.status(200).json({"Firstname":req.body.firstname,"Lastname":req.body.lastname,"Username":req.body.username,"Phone":req.body.phone,"Other":req.body.other});
       });
